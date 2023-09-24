5645178cfc1679153c3e0bab95282b8c319fccfe
Naol Melesse <naolmelesse91@gmail.com>
2023-09-24 17:02:35 +0300

    resolved a bug

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('HOST_URL'), // Sets the public URL of the application.
+  url: env('RENDER_EXTERNAL_URL'), // Sets the public URL of the application.
   app: {
     keys: env.array('APP_KEYS')
   },
