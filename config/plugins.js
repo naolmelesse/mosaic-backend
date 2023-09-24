d61305011888b5c77e8967e5910eb8b0e181c0f6
Naol Melesse <naolmelesse91@gmail.com>
2023-09-24 18:39:23 +0300

    cloudinary issue resolve trial

--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'cloudinary',
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+          cloud_name: 'dgfd4wb8d',
+          api_key: '928747199988742',
+          api_secret: '3V5Us7nJiTa3EUIeGpp5ePOzTOQ',
         },
         actionOptions: {
           upload: {},
